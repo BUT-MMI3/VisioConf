a87f0f5e9c9fd7b663c25cb9d6ac798391f82d61
Mathis Lambert <mathislambert.dev@gmail.com>
2023-09-06 11:44:46 +0200

    [+] Init React/Vite App and Express server

---
+++ react-front/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/home/Home.jsx";
+import "./styles/index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
