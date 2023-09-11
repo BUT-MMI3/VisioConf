878e72cf334dbb0847f5446f3edfba40f8fcfeae
Mathis Lambert <mathislambert.dev@gmail.com>
2023-09-11 09:50:43 +0200

    [+] updated architecture

--- react-front/src/pages/home/Home.jsx
+++ react-front/src/pages/home/Home.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "/react.svg";
-import viteLogo from "/vite.svg";
 import "./Home.scss";
 
 function Home() {
@@ -8,21 +6,13 @@ function Home() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>MMI VisioConf</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/pages/home/Home.jsx</code> and save to reload.
         </p>
       </div>
     </>
