8947316719992e64973858becc6a051546a176bd
Mathis Lambert <mathis.lambert27@gmail.com>
2024-01-10 12:40:19 +0100

    [+] Vite handling prod or dev environments

--- react-front/src/socket.js
+++ react-front/src/socket.js
@@ -4,9 +4,13 @@
  */
 import { io } from "socket.io-client";
 
+const currentURL = window.location.href;
 // "undefined" means the URL will be computed from the `window.location` object
+console.log(import.meta.env.VITE_ENV === "production");
 const URL =
   // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:3001";
+  import.meta.env.VITE_ENV === "production"
+    ? currentURL.split(":")[0] + ":" + currentURL.split(":")[1] + ":3001"
+    : "http://localhost:3001";
 
 export const socket = io(URL);
