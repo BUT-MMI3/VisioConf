536db776bb6793f701a8539856d564f055702b96
Mathis Lambert <mathis.lambert27@gmail.com>
2024-01-10 22:58:50 +0100

    [+] Added toast system

---
+++ react-front/src/components/Toasts/Toast.jsx
@@ -0,0 +1,36 @@
+import "./toasts.scss";
+
+export default function Toast({ title, message, type }) {
+  return (
+    <div className={`toast ${type}`}>
+      <div className="toast__content">
+        <div className="toast__title">{title}</div>
+        <div className="toast__message">{message}</div>
+      </div>
+      <div className="toast__close">
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.5 1.5L1.5 11.5"
+            stroke="white"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+          />
+          <path
+            d="M1.5 1.5L11.5 11.5"
+            stroke="white"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
---
+++ react-front/src/components/Toasts/ToastContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useCallback, useRef } from "react";
+import { Toasts } from "./Toasts";
+
+// eslint-disable-next-line no-unused-vars
+const defaultPush = (toast) => {}; // Méthode de base que l'on mettra dans le contexte par défaut
+
+const ToastContext = createContext({
+  pushToastRef: { current: defaultPush },
+});
+
+// On entourera notre application de ce provider pour rendre le toasts fonctionnel
+export function ToastContextProvider({ children }) {
+  const pushToastRef = useRef(defaultPush);
+  return (
+    <ToastContext.Provider value={{ pushToastRef }}>
+      <Toasts context={ToastContext} />
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useToasts() {
+  const { pushToastRef } = useContext(ToastContext);
+  return {
+    pushToast: useCallback(
+      (toast) => {
+        pushToastRef.current(toast);
+      },
+      [pushToastRef]
+    ),
+  };
+}
