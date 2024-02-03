6635cc3129336b197345f63a6673d269ba9ab33a
Mathieu Romain <mathieuromain13@gmail.com>
2024-02-03 15:40:05 +0100

    Commit des modifications locales avant fusion

--- api-express/routes/events.js
+++ api-express/routes/events.js
@@ -2,6 +2,8 @@
  * Author: @mathis-lambert
  * Date : Janvier 2024
  */
+const uuid = require('uuid');
+
 module.exports = function (io) {
   io.on("connection", (socket) => {
     console.log("User connected");
@@ -23,9 +25,44 @@ module.exports = function (io) {
       console.log("Custom event received:", data);
     });
 
-    socket.on('search-user', (searchInput) => {
-      // Traitez la recherche d'utilisateur ici et envoyez une réponse au client si nécessaire
-      console.log(`Recherche d'utilisateur : ${searchInput}`);
+    socket.on('search-user', async (query) => {
+      try {
+        
+        const results = await User.find({
+          $or: [{ user_firstname: { $regex: query, $options: 'i' } }, { user_lastname: { $regex: query, $options: 'i' } }],
+        });
+
+        // Émettre les résultats de la recherche à l'utilisateur
+        socket.emit('search-results', results);
+      } catch (error) {
+        console.error('Erreur lors de la recherche d\'utilisateur :', error.message);
+      }
+    });
+
+    socket.on('create-group', async (selectedUsers) => {
+      try {
+        const newDiscussion = new Discussion({
+          discussion_uuid: uuid.v4(), // Utilisation de uuid pour générer un identifiant UUID aléatoire
+          discussion_name: null, 
+          discussion_description: null, 
+          discussion_type: 'group',
+          discussion_members: selectedUsers,
+        });
+
+        const savedDiscussion = await newDiscussion.save();
+        io.emit('group-created', savedDiscussion);
+      } catch (error) {
+        console.error('Erreur lors de la création du groupe :', error.message);
+      }
+    });
+
+    socket.on('fetch-users', async () => {
+      try {
+        const usersList = await User.find({}, 'user_firstname user_lastname _id');
+        socket.emit('users-list', usersList);
+      } catch (error) {
+        console.error('Erreur lors de la récupération de la liste des utilisateurs :', error.message);
+      }
     });
 
     // Disconnect event
