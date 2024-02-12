14094dfc59441319bfe4bc2c61b6b7309b89c274
Kyllian Diochon <77027134+EkinOox@users.noreply.github.com>
2024-02-12 11:42:57 +0100

    NoyauBarre de menu revu
    Overlay plus performant

--- react-front/src/components/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
+++ react-front/src/components/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import FeatherIcon from "feather-icons-react";
 import { Link } from "react-router-dom";
 import "./NoyauBarreDeMenu.css";
@@ -7,7 +7,8 @@ import NoyauDeconnexion from "../../components/NoyauDeconnexion/NoyauDeconnexion
 
 const NoyauBarreDeMenu = () => {
   const [overlayVisible, setOverlayVisible] = useState(false);
-  const logoImage = "https://jeremiahhaulin.fr/img/Logo%20MMI%20Toulon.png";
+  const logoImage =
+    "https://jeremiahhaulin.fr/img/Logo%20MMI%20Toulon.png";
   const utilisateur = {
     id: 123,
     nom: "Doe",
@@ -17,12 +18,32 @@ const NoyauBarreDeMenu = () => {
     job: "Etudiant MMI3",
     isConnected: true,
     isAdmin: true,
-    logo: "https://imgv3.fotor.com/images/gallery/a-girl-cartoon-character-with-pink-background-generated-by-cartoon-character-maker-in-Fotor.jpg",
+    logo:
+      "https://imgv3.fotor.com/images/gallery/a-girl-cartoon-character-with-pink-background-generated-by-cartoon-character-maker-in-Fotor.jpg",
   };
 
   const checkRole = () => {
     return utilisateur.isAdmin ? "Administrateur" : "Utilisateur";
   };
+
+  const overlayRef = useRef(null);
+
+  const handleContainerClick = (event) => {
+    if (
+      overlayVisible &&
+      overlayRef.current &&
+      !overlayRef.current.contains(event.target)
+    ) {
+      setOverlayVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleContainerClick);
+    return () => {
+      document.removeEventListener("mousedown", handleContainerClick);
+    };
+  }, [overlayVisible]);
 
   const handleOverlayToggle = () => {
     setOverlayVisible(!overlayVisible);
@@ -30,7 +51,11 @@ const NoyauBarreDeMenu = () => {
 
   return (
     <div className="barre-de-menu">
-      <img src={logoImage} alt="Logo de l'entreprise" className="logo-band" />
+      <img
+        src={logoImage}
+        alt="Logo de l'entreprise"
+        className="logo-band"
+      />
       <div className="onglets">
         <Link to="/discussions">
           <FeatherIcon
@@ -87,7 +112,7 @@ const NoyauBarreDeMenu = () => {
           className="logo-utilisateur"
         />
         {overlayVisible && (
-          <div className={`profil-overlay`}>
+          <div className={`profil-overlay`} ref={overlayRef}>
             <div className="card">
               <div className="info-container">
                 <img
@@ -109,15 +134,17 @@ const NoyauBarreDeMenu = () => {
                       utilisateur.isConnected ? "connecte" : "deconnecte"
                     }`}
                   />
-                  <p>{utilisateur.isConnected ? "En ligne" : "déconnecté"}</p>
+                  <p>
+                    {utilisateur.isConnected ? "En ligne" : "déconnecté"}
+                  </p>
                 </Link>
 
                 <Link to="parametres" className={"fr g1 ai-c"}>
                   <FeatherIcon
-                      icon="settings"
-                      size="20"
-                      strokeWidth="1"
-                      className="settings"
+                    icon="settings"
+                    size="20"
+                    strokeWidth="1"
+                    className="settings"
                   />
                   <p>Paramètres</p>
                 </Link>
