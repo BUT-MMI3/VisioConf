61f7dd7a3b88b999e942dd3e29ab428a1cf6a167
MATHIS LAMBERT <mathis.lambert27@gmail.com>
2024-03-16 11:19:31 +0100

    [+] now displaying user information in the bottom left corner with session context

--- react-front/src/elements/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
+++ react-front/src/elements/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
@@ -1,162 +1,159 @@
-import { useState, useRef, useEffect } from "react";
+import {useEffect, useRef, useState} from "react";
 import FeatherIcon from "feather-icons-react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "./NoyauBarreDeMenu.css";
 import "../ProfilOverlay/ProfilOverlay.css";
 import NoyauDeconnexion from "../../elements/NoyauDeconnexion/NoyauDeconnexion";
+import {useSelector} from "react-redux";
 
 const NoyauBarreDeMenu = () => {
-  const [overlayVisible, setOverlayVisible] = useState(false);
-  const logoImage =
-    "https://jeremiahhaulin.fr/img/Logo%20MMI%20Toulon.png";
-  const utilisateur = {
-    id: 123,
-    nom: "Doe",
-    prenom: "John",
-    email: "john.doe@example.com",
-    motDePasse: "azerty",
-    job: "Etudiant MMI3",
-    isConnected: true,
-    isAdmin: true,
-    logo:
-      "https://imgv3.fotor.com/images/gallery/a-girl-cartoon-character-with-pink-background-generated-by-cartoon-character-maker-in-Fotor.jpg",
-  };
+    const [overlayVisible, setOverlayVisible] = useState(false);
 
-  const checkRole = () => {
-    return utilisateur.isAdmin ? "Administrateur" : "Utilisateur";
-  };
+    const session = useSelector((state) => state.session);
 
-  const overlayRef = useRef(null);
-
-  const handleContainerClick = (event) => {
-    if (
-      overlayVisible &&
-      overlayRef.current &&
-      !overlayRef.current.contains(event.target)
-    ) {
-      setOverlayVisible(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleContainerClick);
-    return () => {
-      document.removeEventListener("mousedown", handleContainerClick);
+    const checkRole = () => {
+        return session.user_roles[0];
     };
-  }, [overlayVisible]);
 
-  const handleOverlayToggle = () => {
-    setOverlayVisible(!overlayVisible);
-  };
+    const overlayRef = useRef(null);
 
-  return (
-    <div className="barre-de-menu">
-      <img
-        src={logoImage}
-        alt="Logo de l'entreprise"
-        className="logo-band"
-      />
-      <div className="onglets">
-        <Link to="/discussions">
-          <FeatherIcon
-            icon="message-circle"
-            size="40"
-            strokeWidth="1"
-            className="onglet"
-          />
-        </Link>
-        <Link to="/utilisateurs">
-          <FeatherIcon
-            icon="users"
-            size="40"
-            strokeWidth="1"
-            className="onglet"
-          />
-        </Link>
-        <Link to="/dossiers">
-          <FeatherIcon
-            icon="folder"
-            size="40"
-            strokeWidth="1"
-            className="onglet"
-          />
-        </Link>
-        <Link to="/livres">
-          <FeatherIcon
-            icon="book"
-            size="40"
-            strokeWidth="1"
-            className="onglet"
-          />
-        </Link>
-        {checkRole() == "Administrateur" && (
-          <Link to="/admin">
-            <FeatherIcon
-              icon="settings"
-              size="40"
-              strokeWidth="1"
-              className="onglet"
+    const handleContainerClick = (event) => {
+        if (
+            overlayVisible &&
+            overlayRef.current &&
+            !overlayRef.current.contains(event.target)
+        ) {
+            setOverlayVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleContainerClick);
+        return () => {
+            document.removeEventListener("mousedown", handleContainerClick);
+        };
+    }, [overlayVisible]);
+
+    const handleOverlayToggle = () => {
+        setOverlayVisible(!overlayVisible);
+    };
+
+    return (
+        <div className="barre-de-menu">
+            <img
+                src={"./others/mmi-toulon.png"}
+                alt="Logo de l'entreprise"
+                className="logo-band"
             />
-          </Link>
-        )}
-      </div>
-      <div className="profil-section" onClick={handleOverlayToggle}>
-        <div
-          className={`statut-indicateur ${
-            utilisateur.isConnected ? "connecte" : "deconnecte"
-          }`}
-        />
-        <img
-          src={utilisateur.logo}
-          alt="Logo de l'utilisateur"
-          className="logo-utilisateur"
-        />
-        {overlayVisible && (
-          <div className={`profil-overlay`} ref={overlayRef}>
-            <div className="card">
-              <div className="info-container">
-                <img
-                  src={utilisateur.logo}
-                  alt="Logo de l'utilisateur"
-                  className="overlay-logo"
-                />
-                <div className="text-container">
-                  <p
-                    style={{ color: "#223A6A", fontWeight: 600 }}
-                  >{`${utilisateur.nom} ${utilisateur.prenom}`}</p>
-                  <small>{`${utilisateur.job}`}</small>
-                </div>
-              </div>
-              <div className="onglets-overlay">
-                <Link to="/changer-status" className={"fr g1 ai-c"}>
-                  <div
-                    className={`statut-connexion ${
-                      utilisateur.isConnected ? "connecte" : "deconnecte"
-                    }`}
-                  />
-                  <p>
-                    {utilisateur.isConnected ? "En ligne" : "déconnecté"}
-                  </p>
+            <div className="onglets">
+                <Link to="/discussions">
+                    <FeatherIcon
+                        icon="message-circle"
+                        size="40"
+                        strokeWidth="1"
+                        className="onglet"
+                    />
                 </Link>
-
-                <Link to="parametres" className={"fr g1 ai-c"}>
-                  <FeatherIcon
-                    icon="settings"
-                    size="20"
-                    strokeWidth="1"
-                    className="settings"
-                  />
-                  <p>Paramètres</p>
+                <Link to="/utilisateurs">
+                    <FeatherIcon
+                        icon="users"
+                        size="40"
+                        strokeWidth="1"
+                        className="onglet"
+                    />
                 </Link>
-
-                <NoyauDeconnexion />
-              </div>
+                <Link to="/dossiers">
+                    <FeatherIcon
+                        icon="folder"
+                        size="40"
+                        strokeWidth="1"
+                        className="onglet"
+                    />
+                </Link>
+                <Link to="/livres">
+                    <FeatherIcon
+                        icon="book"
+                        size="40"
+                        strokeWidth="1"
+                        className="onglet"
+                    />
+                </Link>
+                {checkRole() === "Administrateur" && (
+                    <Link to="/admin">
+                        <FeatherIcon
+                            icon="settings"
+                            size="40"
+                            strokeWidth="1"
+                            className="onglet"
+                        />
+                    </Link>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+            <div className="profil-section" onClick={handleOverlayToggle}>
+                <div
+                    className={`statut-indicateur ${
+                        session.isSignedIn ? "connecte" : "deconnecte"
+                    }`}
+                />
+                <img
+                    src={session.user_picture}
+                    onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = "./user-icons/user-base-icon.svg";
+                    }}
+                    alt="Logo de l'session"
+                    className="logo-session"
+                />
+                {overlayVisible && (
+                    <div className={`profil-overlay`} ref={overlayRef}>
+                        <div className="card">
+                            <div className="info-container">
+                                <img
+                                    src={session.user_picture}
+                                    onError={(e) => {
+                                        e.target.onerror = null;
+                                        e.target.src = "./user-icons/user-base-icon.svg";
+                                    }}
+                                    alt="Logo de l'session"
+                                    className="overlay-logo"
+                                />
+                                <div className="text-container">
+                                    <p
+                                        style={{color: "#223A6A", fontWeight: 600}}
+                                    >{`${session.user_lastname} ${session.user_firstname}`}</p>
+                                    <small>{`${session.user_job}`}</small>
+                                </div>
+                            </div>
+                            <div className="onglets-overlay">
+                                <Link to="/changer-status" className={"fr g1 ai-c"}>
+                                    <div
+                                        className={`statut-connexion ${
+                                            session.isSignedIn ? "connecte" : "deconnecte"
+                                        }`}
+                                    />
+                                    <p>
+                                        {session.isSignedIn ? "En ligne" : "déconnecté"}
+                                    </p>
+                                </Link>
+
+                                <Link to="parametres" className={"fr g1 ai-c"}>
+                                    <FeatherIcon
+                                        icon="settings"
+                                        size="20"
+                                        strokeWidth="1"
+                                        className="settings"
+                                    />
+                                    <p>Paramètres</p>
+                                </Link>
+
+                                <NoyauDeconnexion/>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default NoyauBarreDeMenu;
