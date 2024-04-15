5a14bf61be52c0ee066e0a69cc4e89e8493e6889
Arthur Mondon <arthur@mondon.pro>
2024-04-15 19:29:23 +0200

    Fix to adjust a renamed variable : from initConnection to appInstance

--- react-front/src/elements/AdminAccueil/AdminAccueil.jsx
+++ react-front/src/elements/AdminAccueil/AdminAccueil.jsx
@@ -1,7 +1,7 @@
 import "./AdminAccueil.scss";
 import FeatherIcon from "feather-icons-react";
 import {useEffect, useRef, useState} from "react";
-import {initConnection} from "../../controller/index.js";
+import {appInstance} from "../../controller/index.js";
 import LinkTo from "../LinkTo/LinkTo.jsx";
 
 const listeMessagesEmis = ["fetch-utilisateurs"];
@@ -11,7 +11,7 @@ const AdminAccueil = () => {
     const instanceName = "AdminAccueil";
     const [utilisateurs, setUtilisateurs] = useState([]);
 
-    const [controller] = useState(initConnection.getController());
+    const [controller] = useState(appInstance.getController());
 
 
     const {current} = useRef({
@@ -29,109 +29,109 @@ const AdminAccueil = () => {
     });
 
     return (<div className="admin-accueil layout-content--full">
-            <div className="admin-accueil--card">
-                <h2>Administration</h2>
-            </div>
+        <div className="admin-accueil--card">
+            <h2>Administration</h2>
+        </div>
 
-            <div className="admin-accueil--container">
-                <div className="admin-accueil--cards">
+        <div className="admin-accueil--container">
+            <div className="admin-accueil--cards">
 
-                    <div className="admin-accueil--cards--card admin-accueil--cards--card--connected-users">
-                        <div className={"admin-accueil--cards--card--connected-users--icon"}>
-                            <FeatherIcon
-                                icon="user"
-                                size="40"
-                                strokeWidth="2"
-                            />
-                        </div>
-
-                        <h2>0</h2>
-                        <p>Utilisateurs connectés</p>
+                <div className="admin-accueil--cards--card admin-accueil--cards--card--connected-users">
+                    <div className={"admin-accueil--cards--card--connected-users--icon"}>
+                        <FeatherIcon
+                            icon="user"
+                            size="40"
+                            strokeWidth="2"
+                        />
                     </div>
 
-                    <div className="admin-accueil--cards--card admin-accueil--cards--card--connected-users">
-                        <div className={"admin-accueil--cards--card--connected-users--icon"}>
-                            <FeatherIcon
-                                icon="user"
-                                size="40"
-                                strokeWidth="2"
-                            />
-                        </div>
+                    <h2>0</h2>
+                    <p>Utilisateurs connectés</p>
+                </div>
 
-                        <h2>0</h2>
-                        <p>Utilisateurs inscrits</p>
+                <div className="admin-accueil--cards--card admin-accueil--cards--card--connected-users">
+                    <div className={"admin-accueil--cards--card--connected-users--icon"}>
+                        <FeatherIcon
+                            icon="user"
+                            size="40"
+                            strokeWidth="2"
+                        />
                     </div>
 
-                    <div className="admin-accueil--cards--card admin-accueil--cards--card--calls">
-                        <div className={"admin-accueil--cards--card--calls--icon"}>
-                            <FeatherIcon
-                                icon="phone"
-                                size="40"
-                                strokeWidth="2"
-                            />
-                        </div>
+                    <h2>0</h2>
+                    <p>Utilisateurs inscrits</p>
+                </div>
 
-                        <h2>0</h2>
-                        <p>Appels en cours</p>
+                <div className="admin-accueil--cards--card admin-accueil--cards--card--calls">
+                    <div className={"admin-accueil--cards--card--calls--icon"}>
+                        <FeatherIcon
+                            icon="phone"
+                            size="40"
+                            strokeWidth="2"
+                        />
                     </div>
+
+                    <h2>0</h2>
+                    <p>Appels en cours</p>
                 </div>
             </div>
-            {/*Links to other admin pages*/}
-            <div className="admin-accueil--container">
-                <div className="admin-accueil--links">
-                    <div className="admin-accueil--links--card admin-accueil--links--card--users">
-                        <h2>Utilisateurs</h2>
-                        <div className="admin-accueil--links--card--links">
-                            <LinkTo to="/admin/users" className="admin-accueil--links--link">
-                                <p>Liste des utilisateurs</p>
-                            </LinkTo>
-                            <LinkTo to="/admin/users/new" className="admin-accueil--links--link">
-                                <p>Créer un utilisateur</p>
-                            </LinkTo>
-                        </div>
-
+        </div>
+        {/*Links to other admin pages*/}
+        <div className="admin-accueil--container">
+            <div className="admin-accueil--links">
+                <div className="admin-accueil--links--card admin-accueil--links--card--users">
+                    <h2>Utilisateurs</h2>
+                    <div className="admin-accueil--links--card--links">
+                        <LinkTo to="/admin/users" className="admin-accueil--links--link">
+                            <p>Liste des utilisateurs</p>
+                        </LinkTo>
+                        <LinkTo to="/admin/users/new" className="admin-accueil--links--link">
+                            <p>Créer un utilisateur</p>
+                        </LinkTo>
                     </div>
-
-                    <div className="admin-accueil--links--card admin-accueil--links--card--groups">
-                        <h2>Groupes</h2>
-                        <div className="admin-accueil--links--card--links">
-                            <LinkTo to="/admin/groups" className="admin-accueil--links--link">
-                                <p>Liste des groupes</p>
-                            </LinkTo>
-                            <LinkTo to="/admin/groups/new" className="admin-accueil--links--link">
-                                <p>Créer un groupe</p>
-                            </LinkTo>
-                        </div>
-                    </div>
-
-                    <div className="admin-accueil--links--card admin-accueil--links--card--roles">
-                        <h2>Rôles</h2>
-                        <div className="admin-accueil--links--card--links">
-                            <LinkTo to="/admin/roles" className="admin-accueil--links--link">
-                                <p>Liste des rôles</p>
-                            </LinkTo>
-                            <LinkTo to="/admin/roles/new" className="admin-accueil--links--link">
-                                <p>Créer un Rôle</p>
-                            </LinkTo>
-                        </div>
-                    </div>
-
-                    <div className="admin-accueil--links--card admin-accueil--links--card--permissions">
-                        <h2>Permissions</h2>
-                        <div className="admin-accueil--links--card--links">
-                            <LinkTo to="/admin/permissions" className="admin-accueil--links--link">
-                                <p>Liste des permissions</p>
-                            </LinkTo>
-                            <LinkTo to="/admin/permissions/attribute" className="admin-accueil--links--link">
-                                <p>Attribuer une permission</p>
-                            </LinkTo>
-                        </div>
-                    </div>
-
 
                 </div>
+
+                <div className="admin-accueil--links--card admin-accueil--links--card--groups">
+                    <h2>Groupes</h2>
+                    <div className="admin-accueil--links--card--links">
+                        <LinkTo to="/admin/groups" className="admin-accueil--links--link">
+                            <p>Liste des groupes</p>
+                        </LinkTo>
+                        <LinkTo to="/admin/groups/new" className="admin-accueil--links--link">
+                            <p>Créer un groupe</p>
+                        </LinkTo>
+                    </div>
+                </div>
+
+                <div className="admin-accueil--links--card admin-accueil--links--card--roles">
+                    <h2>Rôles</h2>
+                    <div className="admin-accueil--links--card--links">
+                        <LinkTo to="/admin/roles" className="admin-accueil--links--link">
+                            <p>Liste des rôles</p>
+                        </LinkTo>
+                        <LinkTo to="/admin/roles/new" className="admin-accueil--links--link">
+                            <p>Créer un Rôle</p>
+                        </LinkTo>
+                    </div>
+                </div>
+
+                <div className="admin-accueil--links--card admin-accueil--links--card--permissions">
+                    <h2>Permissions</h2>
+                    <div className="admin-accueil--links--card--links">
+                        <LinkTo to="/admin/permissions" className="admin-accueil--links--link">
+                            <p>Liste des permissions</p>
+                        </LinkTo>
+                        <LinkTo to="/admin/permissions/attribute" className="admin-accueil--links--link">
+                            <p>Attribuer une permission</p>
+                        </LinkTo>
+                    </div>
+                </div>
+
+
             </div>
-        </div>);
+        </div>
+    </div>);
 };
 
 export default AdminAccueil;
