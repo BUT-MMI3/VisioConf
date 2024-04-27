d328bb8205fb963928e33a8ca82d37ab0cebbe1e
Mathis <93223257+mathis-lambert@users.noreply.github.com>
2024-04-27 18:42:43 +0200

    Revert "Revert "Update NoyauBarreDeMenu.css""

--- react-front/src/elements/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
+++ react-front/src/elements/NoyauBarreDeMenu/NoyauBarreDeMenu.jsx
@@ -30,7 +30,7 @@ const NoyauBarreDeMenu = () => {
             if (verbose || controller.verboseall) console.log(`INFO: (${instanceName}) - traitementMessage - `, msg);
 
             if (typeof msg.status_answer !== "undefined") {
-                console.log("Informations utilisateur obtenues");
+                console.log("Informations utilisateur obtenues", msg.status_answer);
                 setStatus(msg.status_answer);
             } else {
                 console.log("Erreur lors du traitement du message :", msg);
@@ -38,18 +38,6 @@ const NoyauBarreDeMenu = () => {
         }
     });
 
-    const logIn = async () => {
-        if (verbose || controller.verboseall) console.log(`INFO: (${instanceName})`);
-
-        return new Promise((resolve, reject) => {
-            try {
-                controller.send(current, {"demande_changement_status": "status utilisateur"});
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    };
     useEffect(() => {
         controller.subscribe(current, listeMessageEmis, listeMessageRecus);
 
@@ -57,10 +45,6 @@ const NoyauBarreDeMenu = () => {
             controller.unsubscribe(current, listeMessageEmis, listeMessageRecus);
         };
     }, [current, controller]);
-    useEffect(() => {
-        logIn();
-    }, []);
-
     const toggleStatus = () => {
         setShowStatus(!showStatus);
     };
@@ -70,16 +54,14 @@ const NoyauBarreDeMenu = () => {
     };
     const handleChangeStatus = async (newStatus) => {
         try {
-            await controller.send(current, { demande_changement_status: newStatus });
-            // Mettre à jour l'état du statut avec le nouveau statut sélectionné
             setSelectedStatus(newStatus);
-
-            controller.send(current, { demande_changement_status: selectedStatus });
-
+            await controller.send(current, { demande_changement_status: newStatus });
+            console.log("Demande de changement de statut envoyée", newStatus)
         } catch (error) {
             console.error("Erreur lors de la demande de changement de statut :", error);
         }
     };
+
 
     const overlayRef = useRef(null);
 
@@ -164,9 +146,14 @@ const NoyauBarreDeMenu = () => {
                 <div className="profil-section" onClick={handleOverlayToggle}>
                     <div
                         className={`statut-indicateur ${
-                            session.isSignedIn ? "connecte" : "deconnecte"
-                        }`}
+                            !status && (
+                                session.user_disturb_status === "available" ? "connecte" :
+                                    session.user_disturb_status === "offline" ? "offline" :
+                                        session.user_disturb_status === "dnd" ? "dnd" : ""
+                            )
+                        } ${status === "available" ? "connecte" : ""} ${status === "offline" ? "offline" : ""} ${status === "dnd" ? "dnd" : ""}`}
                     />
+
                     <img
                         src={session.user_picture}
                         // onError={(e) => {
@@ -198,17 +185,102 @@ const NoyauBarreDeMenu = () => {
                                 </div>
                                 <div className="onglets-overlay">
                                     <div className={"fr g1 ai-c"}>
-                                        <div
-                                            className={`statut-connexion ${
-                                                session.isSignedIn ? "connecte" : "deconnecte"
-                                            }`}
-                                        />
-                                        <p style={{width: "100%",}}>
-                                            {session.isSignedIn ? "En ligne" : "déconnecté"}
+                                        <div className={`statut-connexion`}>
+                                            {!status && (session.user_disturb_status === "available" || session.user_disturb_status === "offline" || session.user_disturb_status === "dnd") && (
+                                                <>
+                                                    {session.user_disturb_status === "available" && (
+                                                        <>
+                                                             <span style={{
+                                                                 fontSize: "2rem",
+                                                                 color: "green"
+                                                             }}>●</span>
+                                                        </>
+                                                    )}
+                                                    {session.user_disturb_status === "offline" && (
+                                                        <>
+                                                            <span style={{
+                                                                fontSize: "2rem",
+                                                                color: "gray"
+                                                            }}>●</span>
+                                                        </>
+                                                    )}
+                                                    {session.user_disturb_status === "dnd" && (
+                                                        <>
+                                                             <span style={{
+                                                                 fontSize: "2rem",
+                                                                 color: "#CB0000"
+                                                             }}>●</span>
+                                                        </>
+                                                    )}
+                                                </>
+                                            )}
+
+                                            {status === "available" && (
+                                                <>
+                                                    <span style={{
+                                                        fontSize: "2rem",
+                                                        color: "green"
+                                                    }}>●</span>
+                                                </>
+                                            )}
+                                            {status === "offline" && (
+                                                <>
+                                                    <span style={{
+                                                        fontSize: "2rem",
+                                                        color: "gray"
+                                                    }}>●</span>
+                                                </>
+                                            )}
+                                            {status === "dnd" && (
+                                                <>
+                                                    <span style={{
+                                                        fontSize: "2rem",
+                                                        color: "#CB0000"
+                                                    }}>●</span>
+                                                </>
+                                            )}
+                                        </div>
+
+                                        <p className={"w-100"}>
+                                            {!status && (session.user_disturb_status === "available" || session.user_disturb_status === "offline" || session.user_disturb_status === "dnd") && (
+                                                <>
+                                                    {session.user_disturb_status === "available" && (
+                                                        <>
+                                                            <span style={{marginLeft: "-1.5rem"}}>En ligne</span>
+                                                        </>
+                                                    )}
+                                                    {session.user_disturb_status === "offline" && (
+                                                        <>
+                                                            <span>Hors ligne</span>
+                                                        </>
+                                                    )}
+                                                    {session.user_disturb_status === "dnd" && (
+                                                        <>
+                                                            <span style={{marginLeft: "0.5rem", whiteSpace: "nowrap",}}>Ne pas déranger</span>
+                                                        </>
+                                                    )}
+                                                </>
+                                            )}
+                                            {status === "available" && (
+                                                <>
+                                                    <span style={{marginLeft: "-1.5rem"}}> En ligne</span>
+                                                </>
+                                            )}
+                                            {status === "offline" && (
+                                                <>
+                                                    <span> Hors ligne</span>
+                                                </>
+                                            )}
+                                            {status === "dnd" && (
+                                                <>
+                                                    <span style={{marginLeft: "0.5rem", whiteSpace: "nowrap",}}> Ne pas déranger</span>
+                                                </>
+                                            )}
                                         </p>
 
                                         <div className="status-affiche w-100 fr jc-c ai-c"
-                                             onClick={toggleStatus}>
+                                             style={{marginRight: "-1rem",}}
+                                             onMouseOver={toggleStatus}>
                                             <FeatherIcon
                                                 icon={showStatus ? 'chevron-down' : 'chevron-right'}
                                                 size="20"
@@ -218,11 +290,40 @@ const NoyauBarreDeMenu = () => {
                                         </div>
 
                                         {showStatus && (
-                                            <div className="section-status-hidden">
+                                            <div className="section-status-hidden w-100">
                                                 <ul>
                                                     {["available", "offline", "dnd"].map((statusOption) => (
-                                                        <li key={statusOption} onClick={() => handleChangeStatus(statusOption)}>
-                                                            {statusOption}
+                                                        <li key={statusOption} className={"fr"}
+                                                            onClick={() => handleChangeStatus(statusOption)}>
+                                                            {statusOption === "available" && (
+                                                                <>
+                                                                    <span style={{
+                                                                        marginLeft: "0.5rem",
+                                                                        color: "green"
+                                                                    }}>●</span>
+                                                                    <span
+                                                                        style={{marginLeft: "0.5rem"}}> En ligne</span>
+                                                                </>
+                                                            )}
+                                                            {statusOption === "offline" && (
+                                                                <>
+                                                                    <span style={{
+                                                                        marginLeft: "0.5rem",
+                                                                        color: "grey"
+                                                                    }}>●</span>
+                                                                    <span
+                                                                        style={{marginLeft: "0.5rem"}}> Hors ligne</span>
+                                                                </>
+                                                            )}
+                                                            {statusOption === "dnd" && (
+                                                                <>
+                                                                    <span style={{
+                                                                        marginLeft: "0.5rem",
+                                                                        color: "#CB0000"
+                                                                    }}>●</span>
+                                                                    <span style={{marginLeft: "0.5rem", whiteSpace: "nowrap",}}> Ne pas déranger</span>
+                                                                </>
+                                                            )}
                                                         </li>
                                                     ))}
                                                 </ul>
