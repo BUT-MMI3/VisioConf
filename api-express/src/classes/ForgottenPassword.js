f6b531e24cdb88040535789c7dd273d6085fc28d
Kyllian Diochon <77027134+EkinOox@users.noreply.github.com>
2024-06-09 20:54:05 +0200

    Refactor and Update
    - Refactor of the PassForgotten.js into ForgottenPassword.js
    
    - Using JWT to create token but the "." to make the salt into the Token has been removed because with the "." pages not working well

--- api-express/src/classes/ForgottenPassword.js
+++ api-express/src/classes/ForgottenPassword.js
@@ -1,21 +1,21 @@
-// PassForgotten.js
+// ForgottenPassword.js
 require("dotenv").config();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const mailer = require('../utils/mailer');
 const {v4: uuidv4} = require("uuid");
 
-class PassForgotten {
+class ForgottenPassword {
     constructor(controller) {
         this.controller = controller;
-        this.instanceName = "PassForgotten";
+        this.instanceName = "ForgottenPassword";
 
         this.listeMessagesEmis = ["password_acceptee", "password_refusee"];
         this.listeMessagesRecus = ["demande_password"];
 
         this.verbose = true;
 
-        if (this.verbose || this.controller.verboseall) console.log("INFO (" + this.instanceName + ") - Création de l'instance PassForgotten : " + this.instanceName);
+        if (this.verbose || this.controller.verboseall) console.log("INFO (" + this.instanceName + ") - Création de l'instance ForgottenPassword : " + this.instanceName);
 
         this.controller.subscribe(this, this.listeMessagesEmis, this.listeMessagesRecus);
     }
@@ -39,16 +39,17 @@ class PassForgotten {
                 }
 
                 // Générer un token unique
-                const token = generateRandomToken(8);
+                const token = jwt.sign({user: user.user_email}, process.env.JWT_SECRET, {expiresIn: "1h", algorithm: "HS256"})
+                const safeToken = token.replace(/\./g, '');
 
                 // Ajouter le token au champ `user_tokens.inscription`
                 user.set({
-                    "user_tokens.invitation": token,
+                    "user_tokens.invitation": safeToken,
                 });
                 await user.save();
 
                 // Créer un lien vers la page d'inscription
-                const resetLink = `http://localhost:3000/inscription/${token}`;
+                const resetLink = `http://localhost:3000/inscription/${safeToken}`;
 
                 // Envoyer l'email avec mailer.sendMail
                 await mailer.sendMail({
@@ -58,7 +59,7 @@ class PassForgotten {
                     html: `<p>Bonjour ${user.user_firstname},</p><p>Vous avez demandé à réinitialiser votre mot de passe. Veuillez cliquer sur le lien suivant pour réinitialiser votre mot de passe : <a href="${resetLink}">Réinitialiser le mot de passe</a></p><p>Si vous n'avez pas demandé cette réinitialisation, veuillez ignorer cet email.</p><p>Merci,<br>Votre équipe</p>`
                 });
 
-                if (this.verbose || this.controller.verboseall) console.log(`INFO (${this.instanceName}) - Email de réinitialisation de mot de passe envoyé à ${user.user_email} - Token : ${token}`);
+                if (this.verbose || this.controller.verboseall) console.log(`INFO (${this.instanceName}) - Email de réinitialisation de mot de passe envoyé à ${user.user_email} - Token : ${safeToken}`);
 
                 // Envoyer un message pour indiquer que l'email est envoyé avec succès
                 this.controller.send(this, {
@@ -95,4 +96,4 @@ class PassForgotten {
     };
 }
 
-module.exports = PassForgotten;
+module.exports = ForgottenPassword;
