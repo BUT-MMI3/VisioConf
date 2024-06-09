7523c249a06f8ea4fd2cae0b2ce9815cbdd3325a
MATHIS LAMBERT <mathis.lambert27@gmail.com>
2024-06-09 19:24:38 +0200

    [+] handle paste files but has to be improve

--- react-front/src/elements/ChatInput/ChatInput.jsx
+++ react-front/src/elements/ChatInput/ChatInput.jsx
@@ -11,24 +11,65 @@ import {Send} from "react-feather";
 export default function ChatInput() {
     const {addMessage} = useDiscussion();
     const [newMessage, setNewMessage] = useState("");
+    const [file, setFile] = useState(null);
 
     // Gestion de la soumission du nouveau message
     const handleSubmit = (event) => {
         event.preventDefault();
         if (newMessage.trim()) {
             addMessage(newMessage.trim());
+            // TODO: check the structure
+            /*
+                {
+                    message: newMessage.trim(),
+                    file: file
+                }
+             */
             setNewMessage(""); // Réinitialiser le champ après l'envoi
         }
     };
 
+    // handle paste file
+    const handlePaste = (event) => {
+        event.preventDefault();
+        const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+        for (const item of items) {
+            if (item.kind === 'file') {
+                const file = item.getAsFile();
+                const blob = new Blob([file], {type: file.type});
+                console.log(blob);
+                // send file as buffer
+                console.log(typeof blob);
+                setFile(blob);
+                addMessage(blob);
+            }
+        }
+    }
+
     return (
         <form onSubmit={handleSubmit} className="chat-form">
+            <div className="preview">
+                {file && (
+                    <div className="preview-file">
+                        {file.type.includes("image") && (
+                            <img src={URL.createObjectURL(file)} alt="file"/>
+                        )}
+                        <button
+                            type="button"
+                            onClick={() => setFile(null)}
+                        >
+                            Annuler
+                        </button>
+                    </div>
+                )}
+            </div>
             <input
                 type="text"
                 value={newMessage}
                 onChange={(event) => setNewMessage(event.target.value)}
                 placeholder="Votre message"
                 className="chat-input"
+                onPaste={(event) => handlePaste(event)}
             />
             <button type="submit" className="chat-send">
                 <Send
